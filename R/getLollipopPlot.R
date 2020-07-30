06934e760a56e452b0bd3f9a3f747458e7560d6f
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2020-07-30 13:54:10 -0500

    Updating lollipop plot

--- R/getLollipopPlot.R
+++ R/getLollipopPlot.R
@@ -20,38 +20,45 @@ poblacion <- poblacion %>% mutate("pob_mil" = pobtot/1000, pob_diezmil = pobtot/
 
 data <- read.csv("../data/crimen_latin.csv", sep = ",",
                  stringsAsFactors = FALSE, encoding = "latin-1")
-data$mun_name <- factor(data$mun_name, levels = municipios)
+#data$mun_name <- factor(data$mun_name, levels = municipios)
 data$date <- as.Date(data$date)
 data %>% filter(cassualties == max(cassualties))
 data <- data %>% filter(mun_name != "")
 min <- as.Date("2011-1-1")
 break.vec <- c(seq(from = as.Date("2011-1-1"), to = as.Date("2018-3-1"),
                    by = "month"))
-break.vec <- break.vec[seq(1,length(break.vec),3)]
+break.vec <- break.vec[seq(1,length(break.vec),6)]
 
 data_pob <- data %>% mutate(month = format(date, "%m"), year = format(date, "%Y")) %>% 
   group_by(mun_name, month, year) %>% summarise(total_casualties = sum(cassualties)) %>% 
   inner_join(poblacion, by = c("mun_name" = "nom_mun_may" ))  %>% 
-  mutate(date = as.Date(paste0(year, "-", month, "-01")))
+  mutate(date = as.Date(paste0(year, "-", month, "-01")), 
+         label = paste0(mun_name, " - ", round(pob_mil, digits = 2), "K inh."))
 
 p <- ggplot(data_pob) +
   geom_segment(aes(x=date, xend=date, y=0, yend=total_casualties/pob_mil), color = "grey") +
   geom_point( aes(x=date, y=total_casualties/pob_mil, color=mun_name), size=0.2) +
   scale_color_viridis_d() +
-  facet_wrap(~mun_name, ncol = 2, dir = "v") +
+  facet_wrap(~label, ncol = 2, dir = "v") +
   theme_ipsum() +
   theme(
     legend.position = "none",
     panel.border = element_blank(),
-    panel.spacing = unit(0.1, "lines"),
-    strip.text.x = element_text(size = 8),
-    axis.text.x = element_text(angle = 90, size = 6),
-    axis.text.y = element_text(size = 6)
+    panel.spacing.x = unit(0.8, "lines"),
+    panel.spacing.y = unit(0.3, "lines"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    strip.text.x = element_text(size = 12),
+    axis.text.x = element_text(angle = 90, size = 8),
+    axis.text.y = element_text(size = 10),
+    axis.title.x = element_text(size = 20),
+    axis.title.y = element_text(size = 20)
   ) + xlab("Date") +
   ylab("Casualties") +
+  scale_y_log10(labels = function(x) format(x, scientific = TRUE)) +
   scale_x_date(breaks = break.vec, labels = date_format("%b-%Y"), 
                expand = expand_scale(0, 0))
 
-png(paste0("../figures/lollipop_mty.png"), width = 1950, height = 3000)
+png(paste0("../figures/lollipop_mty.png"), width = 600, height = 1000)
 p
 dev.off()
